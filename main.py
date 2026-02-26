b37587b452ccceddbe00154c441b3f65996e75a6
nahvallejos <34092236+nahv@users.noreply.github.com>
2026-02-26 15:10:05 -0300

    modificación para build onefile

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import webview
+import os
+import sys
 from backend import Api
+
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
 
 if __name__ == '__main__':
     api = Api()
-    window = webview.create_window('Listado de escritos', 'frontend/index.html', js_api=api, width=1200, height=933)
-    webview.start()  # debug=False by default
+    html_path = resource_path("frontend/index.html")
+    window = webview.create_window(
+        'Listado de escritos',
+        html_path,
+        js_api=api,
+        width=1200,
+        height=933
+    )
+    webview.start()
